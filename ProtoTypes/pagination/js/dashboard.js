11655c1dfb4cc64240046679692bfebf504c8740
developer24Sri <88076621+developer24Sri@users.noreply.github.com>
2026-06-04 17:16:41 +0530

    feature:enabled NoofRecordsPerPage and displayingFromandToRecords

--- ProtoTypes/pagination/js/dashboard.js
+++ ProtoTypes/pagination/js/dashboard.js
@@ -2,7 +2,10 @@ import { EmployeeTable } from "./pagination.js";
 
 const table = new EmployeeTable({
     tableBody: "#employeeTableBody",
-    paginationContainer: "#paginationContainer"
+    paginationContainer: "#paginationContainer",
+    summaryContainer: "#summaryContainer",
+    pageSize: "#pageSize",
+    pageSizeOptions: [5,10,15,20,25]
 })
 
 table.init();
--- ProtoTypes/pagination/js/pagination.js
+++ ProtoTypes/pagination/js/pagination.js
@@ -10,11 +10,17 @@ export class EmployeeTable {
 
     constructor({
         tableBody,
-        paginationContainer
+        paginationContainer,
+        summaryContainer,
+        pageSize,
+        pageSizeOptions
     }) {
         // 2. Get the required data's Via API(internal i.e JS)
         this.tableBody = document.querySelector(tableBody);
         this.paginationContainer = document.querySelector(paginationContainer);
+        this.summaryContainer = document.querySelector(summaryContainer);
+        this.pageSize = document.querySelector(pageSize);
+        this.pageSizeOptions = pageSizeOptions;
     }
 
     async init() {
@@ -22,6 +28,8 @@ export class EmployeeTable {
         await this.loadEmployees();
         this.renderTable();
         this.renderPagination();
+        this.renderSummary();
+        this.renderPageSizeSelector();
     }
 
     // 2. Get the required data's Via API(external) and store it in our states
@@ -29,13 +37,10 @@ export class EmployeeTable {
         const response = await apiService.get(`/protected/employees-show.php?page=${this.#currentPage}&limit=${this.#limit}`)
         this.#employees = response.data;
         this.#pagination = response.pagination;
-        // console.log(this.#employees);
-        // console.log(this.#pagination);
     }
 
     // 3. Render the data inside the table
     renderTable() {
-        // console.log(this.#employees);
         this.tableBody.innerHTML = "";
         this.#employees.forEach(employee => {
             this.tableBody.insertAdjacentHTML(
@@ -54,16 +59,17 @@ export class EmployeeTable {
 
     // 4. render the pagination below the table
     renderPagination() {
-        // console.log(this.#pagination);
         this.paginationContainer.innerHTML = "";
         const isFirstPage = this.#currentPage === 1;
         const isLastPage = this.#currentPage === this.#pagination.totalPages;
-        this.paginationContainer.insertAdjacentHTML("afterbegin",
+        this.paginationContainer.insertAdjacentHTML(
+            "afterbegin",
             `<button class="pagination-prev" ${isFirstPage ? "disabled" : ""}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><path d="M164.24,203.76a6,6,0,1,1-8.48,8.48l-80-80a6,6,0,0,1,0-8.48l80-80a6,6,0,0,1,8.48,8.48L88.49,128Z"/></svg>
             </button>`
         )
-        this.paginationContainer.insertAdjacentHTML("afterbegin",
+        this.paginationContainer.insertAdjacentHTML(
+            "afterbegin",
             `<button class="pagination-first" ${isFirstPage ? "disabled" : ""}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><path d="M204.24,203.76a6,6,0,1,1-8.48,8.48l-80-80a6,6,0,0,1,0-8.48l80-80a6,6,0,0,1,8.48,8.48L128.49,128ZM48.49,128l75.75-75.76a6,6,0,0,0-8.48-8.48l-80,80a6,6,0,0,0,0,8.48l80,80a6,6,0,1,0,8.48-8.48Z"/></svg>
             </button>`
@@ -75,16 +81,51 @@ export class EmployeeTable {
                 `<button class="page-btn ${activeClass}" data-page="${i}">${i}</button>`
             )
         }
-        this.paginationContainer.insertAdjacentHTML("beforeend",
+        this.paginationContainer.insertAdjacentHTML(
+            "beforeend",
             `<button class="pagination-next" ${isLastPage ? "disabled" : ""}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><path d="M180.24,132.24l-80,80a6,6,0,0,1-8.48-8.48L167.51,128,91.76,52.24a6,6,0,0,1,8.48-8.48l80,80A6,6,0,0,1,180.24,132.24Z"/></svg>
                 </button>`
         )
-        this.paginationContainer.insertAdjacentHTML("beforeend",
+        this.paginationContainer.insertAdjacentHTML(
+            "beforeend",
             ` <button class="pagination-last" ${isLastPage ? "disabled" : ""}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#000000" viewBox="0 0 256 256"><path d="M140.24,132.24l-80,80a6,6,0,0,1-8.48-8.48L127.51,128,51.76,52.24a6,6,0,0,1,8.48-8.48l80,80A6,6,0,0,1,140.24,132.24Zm80-8.48-80-80a6,6,0,0,0-8.48,8.48L207.51,128l-75.75,75.76a6,6,0,1,0,8.48,8.48l80-80A6,6,0,0,0,220.24,123.76Z"/></svg>
             </button>`
         )
+    }
+
+    // 6. to render the record summary of the employees:
+    renderSummary() {
+        const startText = ((this.#currentPage - 1) * this.#limit) + 1;
+        const endText = Math.min(this.#currentPage * this.#limit, this.#pagination.totalRecords);
+        this.summaryContainer.textContent = `Showing ${startText} to ${endText} from ${this.#pagination.totalRecords} records.`
+    }
+
+    // 7. i) to render the page size select element:
+    renderPageSizeSelector() {
+        this.pageSize.innerHTML = "";
+        this.pageSizeOptions.forEach(size => {
+            this.pageSize.insertAdjacentHTML(
+                "beforeend",
+                `
+                 <option value="${size}">
+                    ${size}
+                </option>
+                `
+            );
+        })
+        this.pageSize.value = this.#limit;
+    }
+
+    // 7. ii) to handle the 
+    async handlePageSizeChange(limit) {
+        this.#limit = limit;
+        this.#currentPage = 1;
+        await this.loadEmployees();
+        this.renderTable();
+        this.renderPagination();
+        this.renderSummary();
     }
 
     // 5 i) Navigate through pages using the currentPage what we get:
@@ -93,6 +134,7 @@ export class EmployeeTable {
         await this.loadEmployees();
         this.renderTable();
         this.renderPagination();
+        this.renderSummary();
     }
 
     // 5 ii) just click the button get the dataset from the button and send it to the handlePage
@@ -103,34 +145,22 @@ export class EmployeeTable {
             const prevButton = event.target.closest(".pagination-prev");
             const lastButton = event.target.closest(".pagination-last");
             const nextButton = event.target.closest(".pagination-next");
-            if (
-                nextButton &&
-                this.#currentPage < this.#pagination.totalPages
-            ) {
-                await this.handlePageChange(
-                    this.#currentPage + 1
-                );
-
+            if (nextButton && this.#currentPage < this.#pagination.totalPages) {
+                await this.handlePageChange(this.#currentPage + 1);
                 return;
             }
 
-            if (
-                prevButton &&
-                this.#currentPage > 1
-            ) {
-                await this.handlePageChange(
-                    this.#currentPage - 1
-                );
-
+            if (prevButton && this.#currentPage > 1) {
+                await this.handlePageChange(this.#currentPage - 1);
                 return;
             }
 
-            if(firstButton) {
+            if (firstButton && this.#currentPage !== 1) {
                 await this.handlePageChange(1);
                 return;
             }
 
-            if(lastButton) {
+            if (lastButton && this.#currentPage !== this.#pagination.totalPages) {
                 await this.handlePageChange(this.#pagination.totalPages);
                 return;
             }
@@ -138,5 +168,13 @@ export class EmployeeTable {
             const page = Number(button.dataset.page);
             await this.handlePageChange(page);
         })
+        this.pageSize.addEventListener(
+            "change",
+            async (event) => {
+                const limit = Number(event.target.value);
+                await this.handlePageSizeChange(limit);
+            }
+        )
     }
+
 }
